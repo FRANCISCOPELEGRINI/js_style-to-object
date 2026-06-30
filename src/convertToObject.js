40dd9dd29fe6e4152e91d8f4a14b1290fc2254c2
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2026-06-29 23:41:53 -0300

    converteToObjectjs 2.0

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,28 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = {};
-
-  const rules = sourceString.split(';');
-
-  for (const rule of rules) {
+  return sourceString.split(';').reduce((stylesObject, rule) => {
     if (!rule.trim()) {
-      continue;
+      return stylesObject;
     }
 
     const index = rule.indexOf(':');
 
     if (index === -1) {
-      continue;
+      return stylesObject;
     }
 
     const property = rule.slice(0, index).trim();
     const value = rule.slice(index + 1).trim();
 
-    result[property] = value;
-  }
-
-  return result;
+    return {
+      ...stylesObject,
+      [property]: value,
+    };
+  }, {});
 }
 
 module.exports = convertToObject;
