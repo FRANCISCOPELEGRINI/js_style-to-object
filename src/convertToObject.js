06b6a6191705246f8700eae0cbe14f38007fb6dd
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2026-06-29 23:39:04 -0300

    js-style-to-object 1.0

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,28 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const rules = sourceString.split(';');
+
+  for (const rule of rules) {
+    if (!rule.trim()) {
+      continue;
+    }
+
+    const index = rule.indexOf(':');
+
+    if (index === -1) {
+      continue;
+    }
+
+    const property = rule.slice(0, index).trim();
+    const value = rule.slice(index + 1).trim();
+
+    result[property] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
